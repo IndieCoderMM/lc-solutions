cafea7610f980b0f25a3911165d383c689582739
Hein Thant <hthant00chk@gmail.com>
2023-08-24 23:48:08 +0630

    Time: 67 ms (30.08%) | Memory: 42.8 MB (47.88%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.js
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.js
@@ -6,31 +6,39 @@
 var gcdOfStrings = function(str1, str2) {
     if (str1[0] !== str2[0]) return "";
 
-    return str1.length > str2.length ? divideStr(str1, str2) : divideStr(str2, str1);
+    return str1.length > str2.length ? findGCD(str1, str2) : findGCD(str2, str1);
 };
 
-const divideStr = (longStr, str) => {
-    let ans = str.split('');
-    while (ans.length > 0) {
-        if (longStr.length % ans.length || str.length % ans.length) {
-            ans.pop();
-            continue;
-        }
-        let j = 0;
-        for (let i = 0; i < longStr.length; i++) {
-            if (longStr[i] !== ans[j]) break;
-            if (i < str.length && str[i] !== ans[j]) break;
-            j = (j+1) % ans.length;
-            if (i === longStr.length - 1) return ans.join('');
-        }
-        ans.pop();
+const findGCD = (longStr, str) => {
+    for (let i = str.length; i > 0; i--) {
+        if (longStr.length % i || str.length % i) continue;
+        const curr = longStr.slice(0, i);
+        const [n1, n2] = [longStr.length / i, str.length / i];
+
+        if (longStr === curr.repeat(n1) && str === curr.repeat(n2)) return curr;
     }
 
-    return ans.join('');
+    return "";
 }
 
-// divisor = ""
-// loop i; i < minLen(str1, str2); i++
-// join to divisor if i1 == i2: divisor += i1; 
-// if i1 != i2: 
-//   
+/* Manual check */
+
+// const divideStr = (longStr, str) => {
+//     let ans = str.split('');
+//     while (ans.length > 0) {
+//         if (longStr.length % ans.length || str.length % ans.length) {
+//             ans.pop();
+//             continue;
+//         }
+//         let j = 0;
+//         for (let i = 0; i < longStr.length; i++) {
+//             if (longStr[i] !== ans[j]) break;
+//             if (i < str.length && str[i] !== ans[j]) break;
+//             j = (j+1) % ans.length;
+//             if (i === longStr.length - 1) return ans.join('');
+//         }
+//         ans.pop();
+//     }
+
+//     return "";
+// }
