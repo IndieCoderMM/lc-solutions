bc3de9b70d0d4fe3c43c512eabce79a2195e511d
Hein Thant <hthant00chk@gmail.com>
2023-08-26 18:18:52 +0630

    Time: 47 ms (94.42%) | Memory: 42.2 MB (71.35%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
@@ -5,11 +5,12 @@
  */
 var kidsWithCandies = function(candies, extraCandies) {
     const greatest = Math.max(...candies);
-    const ans = new Array(candies.length).fill(false);
+    const ans = [];
 
     for (let i = 0; i < candies.length; i++) {
         if (candies[i] + extraCandies >= greatest)  
             ans[i] = true;
+        else ans[i] = false;
     }
 
     return ans;
