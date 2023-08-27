54be4fb27df8e83b064ff5b9f96c5805c7225d8d
Hein Thant <hthant00chk@gmail.com>
2023-08-27 23:12:45 +0630

    Time: 83 ms (7.62%) | Memory: 48 MB (6.24%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    if (n === 0) return true;
+    
+    const helper = [0, ...flowerbed, 0];
+    let plantable = 0;
+
+    for (let i = 1; i < helper.length-1; i++) {
+        if (helper[i-1] + helper[i] + helper[i+1] === 0) {
+            plantable += 1;
+            helper[i] = 1;
+        }
+        if (plantable === n) return true;
+    }
+    console.log(helper);
+
+    return false;
+};
