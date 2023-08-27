9e435a24e10df27baebf3e7de9444542d6b1906f
Hein Thant <hthant00chk@gmail.com>
2023-08-27 23:21:24 +0630

    Time: 50 ms (94.27%) | Memory: 43.6 MB (95.75%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.js
+++ 605-can-place-flowers/can-place-flowers.js
@@ -6,13 +6,15 @@
 var canPlaceFlowers = function(flowerbed, n) {
     if (n === 0) return true;
 
-    const helper = [0, ...flowerbed, 0];
     let plantable = 0;
 
-    for (let i = 1; i < helper.length-1; i++) {
-        if (helper[i-1] + helper[i] + helper[i+1] === 0) {
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 1) continue;
+
+        const sum = (i === 0 ? 0 : flowerbed[i-1]) + (i === flowerbed.length - 1 ? 0 : flowerbed[i+1])
+        if (sum === 0) {
             plantable += 1;
-            helper[i] = 1;
+            flowerbed[i] = 1;
         }
         if (plantable === n) return true;
     }
