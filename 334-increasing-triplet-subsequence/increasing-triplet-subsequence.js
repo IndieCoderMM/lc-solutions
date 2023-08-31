27aba91aa2e8ee467bee2f8549e11415e9253c77
Hein Thant <hthant00chk@gmail.com>
2023-08-31 20:25:36 +0630

    Time: 87 ms (37.86%) | Memory: 61.1 MB (44.13%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.js
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.js
@@ -6,7 +6,6 @@ var increasingTriplet = function(nums) {
    let [a, b] = [Infinity, Infinity];
    
    for (const n of nums) {
-       console.log(a, b, n);
        if (n > b) return true;
        if (n > a && n <= b) b = n;
        else a = n;
