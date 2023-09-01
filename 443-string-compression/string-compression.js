45d45b9b6cc95c6cb814aa489d36d61420e2ccec
Hein Thant <hthant00chk@gmail.com>
2023-09-01 23:44:43 +0630

    Time: 61 ms (68.07%) | Memory: 44.5 MB (49.82%) - LeetSync

--- 443-string-compression/string-compression.js
+++ 443-string-compression/string-compression.js
@@ -3,55 +3,40 @@
  * @return {number}
  */
 var compress = function(chars) {
-    const len = chars.length;
+    // let chars = ["a","a","b","b","c","c","c"]
+    const len = chars.length; // len = 7
     if (len === 0) return [];
 
     let idx = 0;
     let i = 0;
 
+    /* running first loop */
     while (i < len) {
-        const curr = chars[i];
-        let j = i+1;
+        const curr = chars[i]; // curr = 'a'
+        let j = i+1; // j = 1
 
+        /* increment j till different char is found */
         while (chars[j] === curr) {
             j++;
         }
-        const count = j - i;
+        // j = 2
+        const count = j - i; // count = 2
 
-        chars[idx] = chars[i]; 
-        idx += 1;
+        chars[idx] = curr; // chars = ["a","a","b","b","c","c","c"]
+        idx += 1; // idx = 1
 
         if (count > 1) {
             for (let c of count.toString()) {
                 chars[idx] = c;
                 idx += 1;
             }
-        } 
-        i += count;
+        }
+        // chars = ["a","2","b","b","c","c","c"], idx = 2
+        i += count; // i = 2
     }
 
 
-    chars = chars.slice(0, idx)
+    chars = chars.slice(0, idx) // ["a","2","b","2","c","3"]
 
     return chars.length;
 };
-
-// ["a","a","b","b","c","c","c"] => ["a","2","b","2","c","3"]
-// >> 6
-
-// s = "" 
-// 1. s << a2
-// 2. s << b2
-// 3. s << c3
-// chars = [a,2,b,2,c,3]
-// >> len(chars)
-
-// prev, count
-// for each char
-// if ch === prev 
-//   count += 1
-// else
-//   if prev s << prev + count
-//   prev = ch
-//   count = 1;
-// return len(s)
