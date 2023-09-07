a627f2b3efafabd833c79a003c32b80dfb0e4583
Hein Thant <hthant00chk@gmail.com>
2023-09-08 00:05:33 +0630

    Time: 94 ms (53.80%) | Memory: 49 MB (43.53%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.js
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.js
@@ -5,20 +5,19 @@
  */
 var findDifference = function(nums1, nums2) {
     const map = new Map();
-    for (const n of new Set(nums1)) {
+    for (const n of nums1) {
         map.set(n, 1);
     }
 
     for (const n of new Set(nums2)) {
-        if (map.get(n) === 1) map.set(n, 0);
-        else map.set(n, -1);
+        map.set(n, (map.get(n) || 0) -1);
     }
-
+    
     const ans = [[], []];
 
     for (const k of map.keys()) {
-        if (map.get(k) === 1) ans[0].push(k)
-        else if (map.get(k) === -1) ans[1].push(k)
+        if (map.get(k) === 1) ans[0].push(k);
+        else if (map.get(k) < 0) ans[1].push(k);
     }   
 
     return ans;
