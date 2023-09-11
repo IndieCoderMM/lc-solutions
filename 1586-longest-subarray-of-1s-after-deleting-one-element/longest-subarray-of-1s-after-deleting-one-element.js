af123c81bb954f54023f72d9f5b1212cc3f7b01d
Hein Thant <hthant00chk@gmail.com>
2023-09-11 23:26:21 +0630

    Time: 65 ms (54.06%) | Memory: 48.8 MB (13.09%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.js
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var longestSubarray = function(nums) {
-    const len = nums.length;
-    if (nums.filter(n => n === 0).length <= 1) return len - 1;
+    if (nums.filter(n => n === 0).length <= 1) 
+        return nums.length - 1;
 
     let [maxLen, zeroIdx, left] = [0, -1, 0];
 
-    for (let i = 0; i < len; i++) {
+    for (let i = 0; i < nums.length; i++) {
         if (nums[i] === 0) {
             left = zeroIdx + 1;
             zeroIdx = i;
