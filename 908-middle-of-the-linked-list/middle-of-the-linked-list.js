3e546e909826230225a06207a4e2ec2f24c00281
Hein Thant <hthant00chk@gmail.com>
2023-10-24 16:40:10 +0630

    Time: 56 ms (21.97%) | Memory: 42.2 MB (23.36%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    if (!head.next) return head;
+
+    let slow = head, fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next?.next ?? null;
+    }
+
+    return slow;
+};
