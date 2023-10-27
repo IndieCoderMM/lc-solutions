bf4c44baa311974e570f68b8686cdfd24f062fcf
Hein Thant <hthant00chk@gmail.com>
2023-10-27 12:41:51 +0630

    Time: 66 ms (89.05%) | Memory: 43.4 MB (81.54%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -3,24 +3,31 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    for (let len = s.length; len > 0; len--) {
-        for (let i = 0; i + len <= s.length; i++) {
-            let left = i, right = i + len - 1;
-            let isPalindrome = true;
-            while (left < right) {
-                if (s[left] !== s[right]) {
-                    isPalindrome = false;
-                    break;
-                }
-                left += 1;
-                right -= 1;
-            }
+    const len = s.length;
+    let ans = [0, 0];
+    const getLongestLen = (i, j) => {
+        let left = i, right = j;
+        while (left >= 0 && right < len) {
+            if (s[left] !== s[right]) break;
+            left -= 1;
+            right += 1;
+        }
 
-            if (isPalindrome) {
-                return s.substring(i, i + len);
-            }
+        return right - left - 1;
+    }
+
+    for (let i = 0; i < len; i++) {
+        const oddLen = getLongestLen(i, i);
+        if (oddLen > ans[1] - ans[0] + 1) {
+            ans[0] = i - Math.floor(oddLen / 2);
+            ans[1] = i + Math.floor(oddLen / 2);
+        }
+        const evenLen = getLongestLen(i, i + 1);
+        if (evenLen > ans[1] - ans[0] + 1) {
+            ans[0] = i - Math.floor(evenLen / 2) + 1;
+            ans[1] = i + Math.floor(evenLen / 2);
         }
     }
 
-    return "";
+    return s.substring(ans[0], ans[1] + 1);
 };
