f1635b01a248b273dcf94b4a7d8e449baba10d7c
Hein Thant <hthant00chk@gmail.com>
2024-02-10 20:57:13 +0630

    Time: 1483 ms (14.77%) | Memory: 158.6 MB (5.01%) - LeetSync

---
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -0,0 +1,21 @@
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+      nums.sort()
+      memo = {}
+
+      def dfs(i: int, prev: int) -> List[int]:
+        if (i == len(nums)): return []
+        if (i, prev) in memo:
+          return memo[(i, prev)]
+        
+        res = dfs(i+1, prev)
+        if nums[i] % prev == 0:
+          tmp = [nums[i]] + dfs(i+1, nums[i])
+          res = tmp if len(tmp) > len(res) else res
+
+        memo[(i, prev)] = res
+
+        return res
+      
+      return dfs(0, 1)
+         
