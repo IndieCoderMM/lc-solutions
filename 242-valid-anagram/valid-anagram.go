af70b71c27f92d7b4dd80ff5ec09e87bc4d3a202
Hein Thant <hthant00chk@gmail.com>
2024-08-12 18:12:35 +0630

    Time: 8 ms (43.24%) | Memory: 3 MB (59.55%) - LeetSync

--- 242-valid-anagram/valid-anagram.go
+++ 242-valid-anagram/valid-anagram.go
@@ -3,28 +3,15 @@ func isAnagram(s string, t string) bool {
     return false
    } 
 
-   counterS := map[byte]int{}
-   counterT := map[byte]int{}
+   counter := map[byte]int{}
 
     for i := 0; i < len(s); i++ {
-        valS, existS := counterS[s[i]]
-        valT, existT := counterT[t[i]]
-
-        if existS {
-            counterS[s[i]] = valS + 1
-        } else {
-            counterS[s[i]] = 1
-        }
-
-        if existT {
-            counterT[t[i]] = valT + 1
-        } else {
-            counterT[t[i]] = 1
-        }
+        counter[s[i]]++
+        counter[t[i]]--
     }
 
-    for k,v := range counterS {
-        if counterT[k] != v {
+    for _,v := range counter {
+        if v != 0 {
             return false
         }
     }
