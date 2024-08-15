1bed249b90d8ac7c32fe3cc84a5d482e2ee1dfe5
Hein Thant <hthant00chk@gmail.com>
2024-08-15 21:06:02 +0630

    Time: 92 ms (56.25%) | Memory: 8.3 MB (45.31%) - LeetSync

--- 890-lemonade-change/lemonade-change.go
+++ 890-lemonade-change/lemonade-change.go
@@ -2,24 +2,26 @@ func lemonadeChange(bills []int) bool {
     fives, tens := 0, 0
 
     for _, b := range bills {
-        switch b {
-            case 5:
-                fives++
-            case 10:
-                if fives < 0 {
-                    return false
-                }
+        if b == 5 {
+            fives++
+            continue
+        }
+
+        if b == 10 {
+            if fives < 0 {
+                return false
+            }
+            fives--
+            tens++
+        } else if b == 20 {
+            if fives > 0 && tens > 0 {
                 fives--
-                tens++
-            case 20:
-                if fives > 0 && tens > 0 {
-                    fives--
-                    tens--
-                } else if fives >= 3 {
-                    fives -= 3
-                } else {
-                    return false
-                }
+                tens--
+            } else if fives >= 3 {
+                fives -= 3
+            } else {
+                return false
+            }
         }
     }
 
