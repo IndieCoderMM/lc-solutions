db4395158a7fc3de76e15d6fd8e2832ec790e14d
Hein Thant <hthant00chk@gmail.com>
2024-08-18 10:02:57 +0630

    Time: 0 ms (100.00%) | Memory: 7 MB (22.58%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.go
+++ 264-ugly-number-ii/ugly-number-ii.go
@@ -4,34 +4,20 @@ func nthUglyNumber(n int) int {
     i2, i3, i5 := 0, 0, 0
 
     for i := 1; i < n; i++ {
-        min := getMin(nums[i2] * 2, nums[i3] * 3, nums[i5] * 5)
+        next := min(nums[i2] * 2, nums[i3] * 3, nums[i5] * 5)
 
-        nums = append(nums, min)
+        nums = append(nums, next)
 
-        if min == nums[i2] * 2 {
+        if next == nums[i2] * 2 {
             i2++
         }
-        if min == nums[i3] * 3 {
+        if next == nums[i3] * 3 {
             i3++
         }
-        if min == nums[i5] * 5 {
+        if next == nums[i5] * 5 {
             i5++
         }
     }
 
     return nums[n - 1]
-}
-
-func getMin(a, b, c int) int {
-    min := a
-
-    if b < min {
-        min = b
-    }
-
-    if c < min {
-        min = c
-    }
-
-    return min
 }
