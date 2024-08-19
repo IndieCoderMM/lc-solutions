74f11b2708482d1e7fbb3b2dd34225c11a775846
Hein Thant <hthant00chk@gmail.com>
2024-08-19 22:16:38 +0630

    Time: 29 ms (17.65%) | Memory: 13.6 MB (8.82%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.go
+++ 650-2-keys-keyboard/2-keys-keyboard.go
@@ -5,32 +5,34 @@ func minSteps(n int) int {
 
    cache := make(map[[2]int]int)
 
-    return 1 + getMinOps(1, 1, n, cache)
+    var getMinOps func (count, copied int) int
+
+
+    getMinOps = func (count, copied int) int {
+        if count == n {
+            return 0
+        }
+
+        if count > n {
+            return 9999
+        }
+
+        key := [2]int{count, copied}
+
+        if v, ok := cache[key]; ok {
+            return v
+        }
+
+        // Paste the copied (+1 op)
+        a := 1 + getMinOps(count + copied, copied)
+
+        // Copy & paste (+2 op)
+        b := 2 + getMinOps(count * 2, count)
+
+        cache[key] = min(a, b)
+
+        return cache[key]
+    }
+
+    return 1 + getMinOps(1, 1)
 }
-
-func getMinOps(count, copied, target int, cache map[[2]int]int) int {
-    if count == target {
-        return 0
-    }
-
-    if count > target {
-        return 9999
-    }
-
-    key := [2]int{count, copied}
-
-    if v, ok := cache[key]; ok {
-        return v
-    }
-
-    // Paste the copied (+1 op)
-    a := 1 + getMinOps(count + copied, copied, target, cache)
-
-    // Copy & paste (+2 op)
-    b := 2 + getMinOps(count * 2, count, target, cache)
-
-    cache[key] = min(a, b)
-
-    return cache[key]
-}
-
