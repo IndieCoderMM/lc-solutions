d1053e24f7a61b31aa6ddadba54effa49a2ff31a
Hein Thant <hthant00chk@gmail.com>
2024-08-19 22:11:15 +0630

    Time: 37 ms (8.82%) | Memory: 8 MB (8.82%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.go
+++ 650-2-keys-keyboard/2-keys-keyboard.go
@@ -3,10 +3,12 @@ func minSteps(n int) int {
     return 0
    } 
 
-    return 1 + getMinOps(1, 1, n)
+   cache := make(map[[2]int]int)
+
+    return 1 + getMinOps(1, 1, n, cache)
 }
 
-func getMinOps(count, copied, target int) int {
+func getMinOps(count, copied, target int, cache map[[2]int]int) int {
     if count == target {
         return 0
     }
@@ -15,11 +17,20 @@ func getMinOps(count, copied, target int) int {
         return 9999
     }
 
+    key := [2]int{count, copied}
+
+    if v, ok := cache[key]; ok {
+        return v
+    }
+
     // Paste the copied (+1 op)
-    a := 1 + getMinOps(count + copied, copied, target)
+    a := 1 + getMinOps(count + copied, copied, target, cache)
 
     // Copy & paste (+2 op)
-    b := 2 + getMinOps(count * 2, count, target)
+    b := 2 + getMinOps(count * 2, count, target, cache)
 
-    return min(a, b)
+    cache[key] = min(a, b)
+
+    return cache[key]
 }
+
