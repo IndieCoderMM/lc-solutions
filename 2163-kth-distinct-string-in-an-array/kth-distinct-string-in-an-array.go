fd6267c2163a2377c46fc73100286b071e062941
Hein Thant <hthant00chk@gmail.com>
2024-08-30 23:16:50 +0630

    Time: 4 ms (81.12%) | Memory: 6.1 MB (19.95%) - LeetSync

---
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.go
@@ -0,0 +1,28 @@
+func kthDistinct(arr []string, k int) string {
+    dict := make(map[string]int)
+    values := make([]string, len(arr))
+
+    for i, s := range arr {
+        _, dupe := dict[s]
+        values[i] = s
+        if dupe {
+            dict[s] = -1
+            continue
+        } 
+        dict[s] = i
+    }
+    print(dict)
+
+    ans := 0
+    for _, s := range values {
+        if dict[s] == -1 {
+            continue
+        }
+        ans++
+        if ans == k {
+            return s
+        }
+    }
+
+    return ""
+}
