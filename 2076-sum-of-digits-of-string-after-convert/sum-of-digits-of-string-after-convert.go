78259d2eff9c6be82d51d1cab85234d45321db17
Hein Thant <hthant00chk@gmail.com>
2024-09-03 21:59:38 +0630

    Time: 2 ms (67.44%) | Memory: 2.4 MB (37.21%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.go
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.go
@@ -6,14 +6,12 @@ import(
 
 func getLucky(s string, k int) int {
     var nums strings.Builder
-    ans := 0
+    var ans int = 0
 
     for _, c := range s {
         n := int(c - 96)
         nums.WriteString(strconv.Itoa(n))
     }
-
-    fmt.Println(nums.String()) 
 
     curr := nums.String()
     for i := 0; i < k; i++ {
@@ -23,17 +21,7 @@ func getLucky(s string, k int) int {
         }
         ans = sum
         curr = strconv.Itoa(ans)
-    fmt.Println(curr) 
     }
 
     return ans 
-}
-
-func getValue(n int, pos int) int {
-    tens := 1
-    for j := 0; j < pos; j++ {
-        tens *= 10
-    } 
-
-    return n * tens
 }
