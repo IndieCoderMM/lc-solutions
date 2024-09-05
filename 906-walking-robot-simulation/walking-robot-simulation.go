5cbe6aa936d02140a979bad500757b6d8ceb5ba2
Hein Thant <hthant00chk@gmail.com>
2024-09-05 18:16:33 +0630

    Time: 59 ms (46.15%) | Memory: 7.2 MB (38.46%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.go
+++ 906-walking-robot-simulation/walking-robot-simulation.go
@@ -1,20 +1,19 @@
 import (
     "fmt"
 )
-type Coor [2]int
 
 func robotSim(commands []int, obstacles [][]int) int {
    dir := [4][2]int{{0,1}, {1,0}, {0, -1}, {-1, 0}}
-   obsMap := make(map[Coor]bool, len(obstacles))
+   obsMap := make(map[string]bool, len(obstacles))
    d := 0 // North
    maxDist := 0
    x, y := 0, 0
 
    for _, o := range obstacles {
-    obsMap[Coor{o[0], o[1]}] = true
+    k := fmt.Sprintf("%d-%d", o[0], o[1])
+    obsMap[k] = true
    }
 
-    fmt.Println(obsMap)
    for _, c := range commands {
     if c == -1 {
         d += 1
@@ -29,22 +28,20 @@ func robotSim(commands []int, obstacles [][]int) int {
     }
 
     currDir := dir[d]
-    fmt.Println(currDir)
     for i := 0; i < c; i++ {
         x += currDir[0]
         y += currDir[1]
-        if _, blocked := obsMap[Coor{x,y}]; blocked {
+        k := fmt.Sprintf("%d-%d", x, y)
+        if _, blocked := obsMap[k]; blocked {
             x -= currDir[0]
             y -= currDir[1]
             break
         }
         dist := x * x + y * y 
-        fmt.Println("Dist: ", dist)
         if dist > maxDist {
             maxDist = dist
         }
     }
-
    }
    
     return maxDist
